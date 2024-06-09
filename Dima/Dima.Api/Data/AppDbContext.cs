1daedcc1e1850178d903aa864e7741e59204fd8b
Ivan Clay <ivan.clay.moura@gmail.com>
2024-06-09 16:48:07 -0300

    Created EF mappings and migrations

---
+++ Dima/Dima.Api/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Dima.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Dima.Api.Data;
+
+public class AppDbContext: DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+    :base(options)
+    {
+        
+    }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
---
+++ Dima/Dima.Api/Data/Mappings/TransactionMapping.cs
@@ -0,0 +1,39 @@
+﻿using Dima.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dima.Api.Data.Mappings;
+
+public class TransactionMapping : IEntityTypeConfiguration<Transaction>
+{
+    public void Configure(EntityTypeBuilder<Transaction> builder)
+    {
+        builder.ToTable("Transaction");
+        
+        builder.HasKey(t => t.Id);
+        builder
+            .Property(t => t.Title)
+            .IsRequired()
+            .HasColumnType("NVARCHAR")
+            .HasMaxLength(80);
+        builder
+            .Property(t => t.Type)
+            .IsRequired()
+            .HasColumnType("SMALLINT");
+        builder
+            .Property(t => t.Amount)
+            .IsRequired()
+            .HasColumnType("MONEY");
+        builder
+            .Property(t => t.CreatedAt)
+            .IsRequired();
+        builder
+            .Property(t => t.PaidOrReceivedAt)
+            .IsRequired(false);
+        builder
+            .Property(t => t.UserId)
+            .IsRequired()
+            .HasColumnType("NVARCHAR")
+            .HasMaxLength(255);
+    }
+}
