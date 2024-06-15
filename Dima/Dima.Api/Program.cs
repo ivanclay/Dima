6660b528b53d5b14d94f16fb79b0471c90f3a02b
Ivan Clay <ivan.clay.moura@gmail.com>
2024-06-15 20:23:48 -0300

    Creating identity mappings

--- Dima/Dima.Api/Program.cs
+++ Dima/Dima.Api/Program.cs
@@ -5,9 +5,16 @@ using Dima.Core.Handlers;
 using Dima.Core.Models;
 using Dima.Core.Requests.Categories;
 using Dima.Core.Responses;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x => 
+{
+    x.CustomSchemaIds(n => n.FullName);
+});
 
 var cnnStr = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
 
@@ -17,13 +24,13 @@ builder.Services.AddDbContext<AppDbContext>(
         x.UseSqlServer(cnnStr);
     });
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(x => 
-{
-    x.CustomSchemaIds(n => n.FullName);
-});
 
 builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
+
+builder.Services
+    .AddAuthentication(IdentityConstants.ApplicationScheme)
+    .AddIdentityCookies();
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
