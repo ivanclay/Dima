a08cd28d9d1e040de53c8f320a1614cb0686ee19
Ivan Clay <ivan.clay.moura@gmail.com>
2024-06-16 12:24:50 -0300

    Organized structures

---
+++ Dima/Dima.Api/Endpoints/Identity/LogoutEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using Dima.Api.Common.Api;
+using Dima.Api.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Dima.Api.Endpoints.Identity;
+
+public class LogoutEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+        => app
+            .MapPost("/logout", HandlerAsync)
+            .RequireAuthorization();
+
+    private static async Task<IResult> HandlerAsync(
+    SignInManager<User> signInManager)
+    {
+        await signInManager.SignOutAsync();
+        return Results.Ok();
+    }
+}
