893cc20d7d87623975aeefa6424c31f0d933095f
Ivan Clay <ivan.clay.moura@gmail.com>
2024-06-17 20:41:41 -0300

    Created layout dark and light

---
+++ Dima/Dima.Web/Configuration.cs
@@ -0,0 +1,38 @@
+﻿using MudBlazor;
+using MudBlazor.Utilities;
+
+namespace Dima.Web;
+
+public static class Configuration
+{
+    public static MudTheme Theme = new MudTheme 
+    {
+        Typography = new()
+        {
+            Default = new Default
+            {
+                FontFamily = ["Raleway", "sans-serif"]
+            }
+        },
+        Palette = new PaletteLight
+        {
+            Primary = new MudColor("#1EFA2D"),
+            Secondary = Colors.LightGreen.Darken3,
+            Background = Colors.Grey.Lighten4,
+            AppbarBackground = new MudColor("#1EFA2D"),
+            AppbarText = Colors.Shades.Black,
+            TextPrimary = Colors.Shades.Black,
+            PrimaryContrastText = Colors.Shades.Black,
+            DrawerText = Colors.Shades.Black,
+            DrawerBackground = Colors.LightGreen.Lighten4
+        },
+        PaletteDark = new PaletteDark
+        {
+            Primary = Colors.LightGreen.Accent3,
+            Secondary = Colors.LightGreen.Darken3,
+            Background = Colors.Grey.Darken4,
+            AppbarBackground = Colors.LightGreen.Accent3,
+            AppbarText = Colors.Shades.Black
+        },
+    };
+}
