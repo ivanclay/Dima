a24d4d36ff1202d6be1686060a2613e4022ad3e7
Ivan Clay <ivan.clay.moura@gmail.com>
2024-07-28 17:54:57 -0300

    Layout and logout redirect

--- Dima/Dima.Web/Pages/Identity/Login.razor.cs
+++ Dima/Dima.Web/Pages/Identity/Login.razor.cs
@@ -55,6 +55,10 @@ public partial class LoginPage : ComponentBase
             if (result.IsSuccess)
             {
                 Snackbar.Add(result.Message, Severity.Success);
+                
+                await AuthenticationStateProvider.GetAuthenticationStateAsync();
+                AuthenticationStateProvider.NotifyAuthenticationStateChanged();
+
                 NavigationManager.NavigateTo("/");
             }
             else
