74549aea59107e26b004f6fb630d9d10704d89d8
Ivan Clay <ivan.clay.moura@gmail.com>
2024-07-28 21:57:58 -0300

    GetByPeriodAsync

--- Dima/Dima.Web/Handlers/TransactionHandler.cs
+++ Dima/Dima.Web/Handlers/TransactionHandler.cs
@@ -1,34 +1,52 @@
-﻿using Dima.Core.Handlers;
+﻿using Dima.Core.Common.Extensions;
+using Dima.Core.Handlers;
 using Dima.Core.Models;
 using Dima.Core.Requests.Transactions;
 using Dima.Core.Responses;
+using System.Net.Http.Json;
 
 namespace Dima.Web.Handlers;
 
-public class TransactionHandler : ITransactionHandler
+public class TransactionHandler(IHttpClientFactory httpClientFactory) : ITransactionHandler
 {
-    public Task<ResponseBase<Transaction?>> CreateAsync(CreateTransactionRequest request)
+    private readonly HttpClient _client = httpClientFactory.CreateClient(Configuration.HttpClientName);
+    
+    public async Task<ResponseBase<Transaction?>> CreateAsync(CreateTransactionRequest request)
     {
         throw new NotImplementedException();
     }
 
-    public Task<ResponseBase<Transaction?>> DeleteAsync(DeleteTransactionRequest request)
+    public async Task<ResponseBase<Transaction?>> DeleteAsync(DeleteTransactionRequest request)
     {
         throw new NotImplementedException();
     }
 
-    public Task<ResponseBase<Transaction?>> GetByIdAsync(GetByIdTransactionRequest request)
+    public async Task<ResponseBase<Transaction?>> GetByIdAsync(GetByIdTransactionRequest request)
     {
         throw new NotImplementedException();
     }
 
-    public Task<PagedResponse<List<Transaction>>> GetByPeriodAsync(GetByPeriodTransactionRequest request)
+    public async Task<ResponseBase<Transaction?>> UpdateAsync(UpdateTransactionRequest request)
     {
         throw new NotImplementedException();
     }
 
-    public Task<ResponseBase<Transaction?>> UpdateAsync(UpdateTransactionRequest request)
+
+    public async Task<PagedResponse<List<Transaction>?>> GetByPeriodAsync(GetByPeriodTransactionRequest request)
     {
-        throw new NotImplementedException();
+        const string format = "yyyy-MM-dd";
+        var startDate = request.StartDate is not null
+            ? request.StartDate.Value.ToString(format)
+            : DateTime.Now.GetFirstDay().ToString(format);
+
+        var endDate = request.EndDate is not null
+            ? request.EndDate.Value.ToString(format)
+            : DateTime.Now.GetLastDay().ToString(format);
+
+        var url = $"v1/transactions?startDate={startDate}&endDate={endDate}";
+
+        return await _client.GetFromJsonAsync<PagedResponse<List<Transaction>?>>(url)
+            ?? new PagedResponse<List<Transaction>?>(null, 400, "Não foi possível obter as transações");
     }
+
 }
