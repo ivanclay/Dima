8e41288673b0ac32396fba5f17222aac5441363a
Ivan Clay <ivan.clay.moura@gmail.com>
2024-07-28 21:40:47 -0300

    category handlers

--- Dima/Dima.Web/Handlers/CategoryHandler.cs
+++ Dima/Dima.Web/Handlers/CategoryHandler.cs
@@ -2,33 +2,41 @@
 using Dima.Core.Models;
 using Dima.Core.Requests.Categories;
 using Dima.Core.Responses;
+using System.Net.Http.Json;
 
 namespace Dima.Web.Handlers;
 
-public class CategoryHandler : ICategoryHandler
+public class CategoryHandler(IHttpClientFactory httpClientFactory) : ICategoryHandler
 {
-    public Task<ResponseBase<Category?>> CreateAsync(CreateCategoryRequest request)
+    private readonly HttpClient _client = httpClientFactory.CreateClient(Configuration.HttpClientName);
+
+    public async Task<ResponseBase<Category?>> CreateAsync(CreateCategoryRequest request)
     {
-        throw new NotImplementedException();
+        var result = await _client.PostAsJsonAsync("v1/categories", request);
+        return await result.Content.ReadFromJsonAsync<ResponseBase<Category?>>()
+            ?? new ResponseBase<Category?>(null, 400, "Falha ao criar uma categoria.");
     }
 
-    public Task<ResponseBase<Category?>> DeleteAsync(DeleteCategoryRequest request)
+    public async Task<ResponseBase<Category?>> UpdateAsync(UpdateCategoryRequest request)
     {
-        throw new NotImplementedException();
+        var result = await _client.PutAsJsonAsync($"v1/categories/{request.Id}", request);
+        return await result.Content.ReadFromJsonAsync<ResponseBase<Category?>>()
+            ?? new ResponseBase<Category?>(null, 400, "Falha ao atualizar a categoria.");
     }
 
-    public Task<PagedResponse<List<Category>>> GetAllAsync(GetAllCategoryRequest request)
+    public async Task<ResponseBase<Category?>> DeleteAsync(DeleteCategoryRequest request)
     {
-        throw new NotImplementedException();
+        var result = await _client.DeleteAsync($"v1/categories/{request.Id}");
+        return await result.Content.ReadFromJsonAsync<ResponseBase<Category?>>()
+            ?? new ResponseBase<Category?>(null, 400, "Falha ao excluir a categoria.");
     }
 
-    public Task<ResponseBase<Category?>> GetByIdAsync(GetByIdCategoryRequest request)
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<ResponseBase<Category?>> GetByIdAsync(GetByIdCategoryRequest request) 
+        => await _client.GetFromJsonAsync<ResponseBase<Category?>>($"v1/categories/{request.Id}")
+        ?? new ResponseBase<Category?>(null, 400, "Não foi possível obter a categoria.");
 
-    public Task<ResponseBase<Category?>> UpdateAsync(UpdateCategoryRequest request)
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<PagedResponse<List<Category>>> GetAllAsync(GetAllCategoryRequest request) 
+        => await _client.GetFromJsonAsync<PagedResponse<List<Category>>>($"v1/categories/")
+            ?? new PagedResponse<List<Category>>(null, 400, "Não foi possível obter as categorias.");
+
 }
