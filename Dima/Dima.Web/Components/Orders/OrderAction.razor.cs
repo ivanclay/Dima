fd21442a373749c6feef45d140d1f500ed843e77
Ivan Clay <ivan.clay.moura@gmail.com>
2024-08-25 13:52:05 -0300

    added checkout

---
+++ Dima/Dima.Web/Components/Orders/OrderAction.razor.cs
@@ -0,0 +1,117 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Dima.Web.Pages.Orders;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MudBlazor;
+
+namespace Dima.Web.Components.Orders;
+
+public partial class OrderActionComponent : ComponentBase
+{
+    #region Parameters
+
+    [CascadingParameter] public DetailsPage Parent { get; set; } = null!;
+
+    [Parameter, EditorRequired] public Order Order { get; set; } = null!;
+
+    #endregion
+
+    #region Services
+
+    [Inject] public IJSRuntime JsRuntime { get; set; } = null!;
+    [Inject] private IDialogService DialogService { get; set; } = null!;
+    [Inject] private IOrderHandler OrderHandler { get; set; } = null!;
+    //[Inject] public IStripeHandler StripeHandler { get; set; } = null!;
+    [Inject] private ISnackbar Snackbar { get; set; } = null!;
+
+    #endregion
+
+    #region Public Methods
+
+    public async void OnCancelButtonClicked()
+    {
+        bool? result = await DialogService.ShowMessageBox(
+            "ATENÇÃO",
+            "Deseja realmente cancelar este pedido?",
+            yesText: "SIM", cancelText: "NÃO");
+
+        if (result is not null && result == true)
+            await CancelOrderAsync();
+    }
+
+    public async void OnPayButtonClicked()
+    {
+        await PayOrderAsync();
+    }
+
+    public async void OnRefundButtonClicked()
+    {
+        bool? result = await DialogService.ShowMessageBox(
+            "ATENÇÃO",
+            "Deseja realmente solicitar o estorno deste pedido?",
+            yesText: "SIM", cancelText: "NÃO");
+
+        if (result is not null && result == true)
+            await RefundOrderAsync();
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private async Task CancelOrderAsync()
+    {
+        var request = new CancelOrderRequest
+        {
+            Id = Order.Id
+        };
+
+        var result = await OrderHandler.CancelAsync(request);
+        if (result.IsSuccess)
+            Parent.RefreshState(result.Data!);
+        else
+            Snackbar.Add(result.Message, Severity.Error);
+    }
+
+    private async Task RefundOrderAsync()
+    {
+        var request = new RefundOrderRequest
+        {
+            Id = Order.Id
+        };
+
+        var result = await OrderHandler.RefundAsync(request);
+        if (result.IsSuccess)
+            Parent.RefreshState(result.Data!);
+        else
+            Snackbar.Add(result.Message, Severity.Error);
+    }
+
+    private async Task PayOrderAsync()
+    {
+        //var request = new CreateSessionRequest
+        //{
+        //    OrderNumber = Order.Number,
+        //    OrderTotal = (int)(Math.Round(Order.Total, 2) * 100),
+        //    ProductTitle = Order.Product.Title,
+        //    ProductDescription = Order.Product.Description
+        //};
+
+        //try
+        //{
+        //    var result = await StripeHandler.CreateSessionAsync(request);
+        //    if (result.Data is not null)
+        //        await JsRuntime.InvokeVoidAsync("checkout", Configuration.StripePublicKey, result.Data);
+        //    else
+        //        Snackbar.Add("Não foi possível iniciar seu pagamento", Severity.Error);
+        //}
+        //catch
+        //{
+        //    Snackbar.Add("Não foi possível iniciar seu pagamento", Severity.Error);
+        //}
+    }
+
+    #endregion
+}
---
+++ Dima/Dima.Web/Pages/Products/List.razor.cs
@@ -0,0 +1,53 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Dima.Web.Pages.Products;
+
+public partial class ListProductsPage : ComponentBase
+{
+    #region Properties
+
+    public bool IsBusy { get; set; } = false;
+    public List<Product> Products { get; set; } = [];
+
+    #endregion
+
+    #region Services
+
+    [Inject]
+    public ISnackbar Snackbar { get; set; } = null!;
+
+    [Inject]
+    public IProductHandler Handler { get; set; } = null!;
+
+    #endregion
+
+    #region Overrides
+
+    protected override async Task OnInitializedAsync()
+    {
+        IsBusy = true;
+        try
+        {
+            var request = new GetAllProductsRequest();
+            var result = await Handler.GetAllAsync(request);
+            if (result.IsSuccess)
+                Products = result.Data ?? [];
+            else
+                Snackbar.Add(result.Message, Severity.Error);
+        }
+        catch (Exception ex)
+        {
+            Snackbar.Add(ex.Message, Severity.Error);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    #endregion
+}
